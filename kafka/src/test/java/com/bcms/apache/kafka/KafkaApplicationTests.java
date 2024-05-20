8fec419fd45629dc7a38222978f7e26ea49a99f0
bcmes <brunomeloesilva@gmail.com>
2024-05-20 15:36:10 -0400

    Initial commit

---
+++ kafka/src/test/java/com/bcms/apache/kafka/KafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bcms.apache.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
